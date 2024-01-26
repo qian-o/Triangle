4594335c9644da28d6e2a15f90dae243f0e02939
王先生 <15718810177@163.com>
2024-01-26 10:59:51 +0800

    wx

--- Source/Triangle.Core/TrContext.cs
+++ Source/Triangle.Core/TrContext.cs
@@ -3,9 +3,9 @@ using Triangle.Core.Contracts;
 
 namespace Triangle.Core;
 
-public class TrContext(GL gL) : Disposable
+public class TrContext(GL gl) : Disposable
 {
-    public GL GL { get; } = gL;
+    public GL GL { get; } = gl;
 
     protected override void Destroy(bool disposing = false)
     {
