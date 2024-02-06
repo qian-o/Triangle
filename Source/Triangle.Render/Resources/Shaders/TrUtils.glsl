85e1b3a0b07d897c9a6f68f5777f27df8c2e903d
王先生 <15718810177@163.com>
2024-02-06 10:30:51 +0800

    wx

--- Source/Triangle.Render/Resources/Shaders/TrUtils.glsl
+++ Source/Triangle.Render/Resources/Shaders/TrUtils.glsl
@@ -88,12 +88,10 @@ vec4 ObjectToClipPos(vec3 pos)
 
 /// <summary>
 /// Transforms normal from object space to world space.
-/// Uni_Transforms.WorldToObject = inverse(Uni_Transforms.ObjectToWorld)
-/// so we can use right multiplication to transform the normal.
 /// </summary>
 vec3 ObjectToWorldNormal(vec3 normal)
 {
-    return normalize((vec4(normal, 0.0) * Uni_Transforms.WorldToObject).xyz);
+    return normalize((normal * mat3(Uni_Transforms.WorldToObject)));
 }
 
 /// <summary>
